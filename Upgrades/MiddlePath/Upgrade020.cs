96b77f3e7813cfcc5f38d5c512a5ea1e0885ad61
Jonyboylovespie <90563990+Jonyboylovespie@users.noreply.github.com>
2023-07-19 23:23:16 -0400

    Update Upgrade020.cs

--- Upgrades/MiddlePath/Upgrade020.cs
+++ Upgrades/MiddlePath/Upgrade020.cs
@@ -10,6 +10,7 @@ namespace TemplateMonkey.Upgrades.MiddlePath
         public override int Path => MIDDLE;
         public override int Tier => 2;
         public override int Cost => 0;
+        public override string DisplayName => "";
         public override string Description => "";
         public override string Portrait => "";
         public override void ApplyUpgrade(TowerModel tower)
