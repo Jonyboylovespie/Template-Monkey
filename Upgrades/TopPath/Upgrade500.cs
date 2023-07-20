a93db2d0fdafe4a827b5ef068af5b09a8ede0654
Jonyboylovespie <90563990+Jonyboylovespie@users.noreply.github.com>
2023-07-19 23:24:33 -0400

    Update Upgrade500.cs

--- Upgrades/TopPath/Upgrade500.cs
+++ Upgrades/TopPath/Upgrade500.cs
@@ -9,6 +9,7 @@ namespace TemplateMonkey.Upgrades.TopPath
         public override int Path => TOP;
         public override int Tier => 5;
         public override int Cost => 0;
+        public override string DisplayName => "";
         public override string Description => "";
         public override string Portrait => "";
         public override void ApplyUpgrade(TowerModel tower)
