e9a08e1700e6f0a95b4b512319b965be3b57f459
Jonyboylovespie <90563990+Jonyboylovespie@users.noreply.github.com>
2023-07-19 23:22:58 -0400

    Update Upgrade004.cs

--- Upgrades/BottomPath/Upgrade004.cs
+++ Upgrades/BottomPath/Upgrade004.cs
@@ -9,6 +9,7 @@ namespace TemplateMonkey.Upgrades.BottomPath
         public override int Path => BOTTOM;
         public override int Tier => 4;
         public override int Cost => 0;
+        public override string DisplayName => "";
         public override string Description => "";
         public override string Portrait => "";
         public override void ApplyUpgrade(TowerModel tower)
