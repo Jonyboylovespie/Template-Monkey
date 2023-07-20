f90237388d45170f03819795070a14456062fbf4
Jonyboylovespie <90563990+Jonyboylovespie@users.noreply.github.com>
2023-07-19 23:24:25 -0400

    Update Upgrade400.cs

--- Upgrades/TopPath/Upgrade400.cs
+++ Upgrades/TopPath/Upgrade400.cs
@@ -11,6 +11,7 @@ namespace TemplateMonkey.Upgrades.TopPath
         public override int Path => TOP;
         public override int Tier => 4;
         public override int Cost => 0;
+        public override string DisplayName => "";
         public override string Description => "";
         public override string Portrait => "";
         public override void ApplyUpgrade(TowerModel tower)
