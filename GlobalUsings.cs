c678d2a46f1f8f673ef676ea6a4d2e41abb1dbb3
Jonyboylovespie <jonathan.frawley@outlook.com>
2025-03-01 00:38:16 -0500

    update

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -2,6 +2,8 @@
 global using BTD_Mod_Helper.Api.Towers;
 global using BTD_Mod_Helper.Extensions;
 global using Il2CppAssets.Scripts.Models.Towers;
+global using Il2CppAssets.Scripts.Models.Towers.Behaviors.Attack;
+global using Il2CppAssets.Scripts.Models.Towers.Behaviors.Emissions;
 global using Il2CppAssets.Scripts.Models.Towers.Projectiles.Behaviors;
 global using Il2CppAssets.Scripts.Models.TowerSets;
 global using MelonLoader;
