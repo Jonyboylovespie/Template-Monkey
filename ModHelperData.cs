36449499cea181c86de338541735a1f8a1b9ef8f
Jonyboylovespie <90563990+Jonyboylovespie@users.noreply.github.com>
2025-03-09 23:26:51 -0400

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,7 +2,7 @@
 {
     public static class ModHelperData
     {
-        public const string WorksOnVersion = "1.0";
+        public const string WorksOnVersion = "GameVersion";
         public const string Version = "1.0.0";
         public const string Name = "Template Monkey";
         public const string Description =
